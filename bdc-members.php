cd86b464d598971941181af95d82c7d90369f6c9
Elliott Richmond <elliott@squareonemd.co.uk>
2026-03-17 17:54:41 +0000

    header update

--- bdc-members.php
+++ bdc-members.php
@@ -6,7 +6,8 @@
  * Version:           1.0.0
  * Requires at least: 6.9
  * Requires PHP:      8.2
- * Author:            BDC
+ * Author:            Elliott Richmond
+ * Author URI:        https://elliottrichmond.co.uk
  * Text Domain:       bdc-members
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
